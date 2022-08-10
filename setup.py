2aab0226c0234b6f58a83eaf62a2be96107bf54b
Toinsson <toinsson@users.noreply.github.com>
2022-08-10 16:59:09 +0200

    update version in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,15 @@ import io
 with open("requirements.txt", "r") as requirements:
     requirements = requirements.read()
 
+# pip's single-source version method as described here:
+# https://python-packaging-user-guide.readthedocs.io/single_source_version/
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 # create long description from readme for pypi
 here = path.abspath(path.dirname(__file__))
@@ -13,7 +22,7 @@ with io.open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='pysdtw',
-      version='0.0.4',
+      version=find_version('pysdtw', '__init__.py'),,
       description='Torch implementation of Soft-DTW, supports CUDA devices.',
       long_description=long_description,
       long_description_content_type='text/markdown',
